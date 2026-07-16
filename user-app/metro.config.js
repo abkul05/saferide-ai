8aefa53d35e22aebf93256aa248e7bb5b5052447
Adester1 <adityakulkarni528@gmail.com>
2026-07-17 03:33:34 +0530

    Disable unstable package exports in metro.config.js to resolve Firebase component auth registry errors

--- user-app/metro.config.js
+++ user-app/metro.config.js
@@ -5,4 +5,7 @@ const config = getDefaultConfig(__dirname);
 // Add support for resolving .cjs files (required by modern Firebase SDK)
 config.resolver.sourceExts.push('cjs');
 
+// Opt-out of the unstable package exports feature to fix Firebase Auth initialization errors
+config.resolver.unstable_enablePackageExports = false;
+
 module.exports = config;
